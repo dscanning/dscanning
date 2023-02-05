c650574622a0c48def04d1fdad24315e64d64380
David Canning <dscanningphotography@gmail.com>
2023-02-05 12:41:42 -0700

    Delete cookie_orders_with_validation directory

--- cookie_orders_with_validation/server.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, session, redirect, request
-from flask_app import app
-from flask_app.controllers import cookie_orders
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
